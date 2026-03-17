70e19572d7f63976a34fb056b880abc9382fde7f
LimWenYuan <wlim0093@student.monash.edu>
2026-03-17 11:20:09 +0800

    Added web build

--- docs/build/web/flutter_bootstrap.js
+++ docs/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2995802950" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "349802689" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
